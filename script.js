8c1a70a47fca134e224009c7dcf5256ebf0774cb
AbineshV <100035495+AbineshV@users.noreply.github.com>
2023-12-09 01:55:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+let newGame = document.querySelector(".new-game");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+
+const winningChances = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    })
+}
+)
+
+const disableBoxes = () => {
+    for (box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+
+const showWinner = (winner) => {
+    msg.innerText = 'Congratulations, Winner is ${winner} ';
+    msgcontainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner = () => {
+    for (let i of winningChances) {
+        let pos1Val = boxes[i[0]].innerText;
+        let pos2Val = boxes[i[1]].innerText;
+        let pos3Val = boxes[i[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("winner ", pos1Val);
+                // let winner = pos1Val;
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgcontainer.classList.add("hide");
+}
+resetBtn.addEventListener("click", resetGame);
+newGame.addEventListener("click", resetGame);
